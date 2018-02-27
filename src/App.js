23d75b1bd820f50529e01300db5abfa70f6e916b
Raghu <mypersonallogins@gmail.com>
2018-02-28 01:35:39 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -19,19 +19,32 @@ const ReactMain = (props) => {
 const Card = (props) => {
   return (
     <div style={{margin: '1em'}}>
-      <img src="http://placehold.it/75"/>
+      <img width="75" src={props.avatar_url}/>
       <div style={{display:'inline-block', marginLeft:10}}>
-        <div style={{fontSize:'1.25em',fontWeight:'bold'}}>Name goes here...</div>
-        <div>Information goes here...</div>
+        <div style={{fontSize:'1.25em',fontWeight:'bold'}}>{props.name}</div>
+        <div>{props.company}</div>
       </div>
     </div>
   );
 }
 
+let data = [
+  {
+    name:"RaghottamK",
+    avatar_url:"https://avatars3.githubusercontent.com/u/23549690?v=4",
+    company:"Cerner"
+  },
+  {
+    name:"Pooja Bhaumik",
+    avatar_url:"https://avatars3.githubusercontent.com/u/19394896?v=4",
+    company:"xxCode"
+  }
+]
+
 const CardList = (props) => {
   return (
     <div>
-      <Card />
+     {props.cards.map(card => <Card {...card} /> )}
     </div>
   )
 }
@@ -44,7 +57,7 @@ class App extends Component {
     return (
       <div>
         <ReactMain />
-        <CardList />
+        <CardList cards={data} />
       </div>      
     );
   }
