cd7a758bf0f3226e7d2fd8b05366ebb78d0d0ddb
Kulkarni <mypersonallogins@gmail.com>
2018-02-28 18:31:01 +0530

    fething user info

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 import logo from './logo.svg';
 import './App.css';
 
@@ -28,19 +29,6 @@ const Card = (props) => {
   );
 }
 
-let data = [
-  {
-    name:"RaghottamK",
-    avatar_url:"https://avatars3.githubusercontent.com/u/23549690?v=4",
-    company:"Cerner"
-  },
-  {
-    name:"Pooja Bhaumik",
-    avatar_url:"https://avatars3.githubusercontent.com/u/19394896?v=4",
-    company:"xxCode"
-  }
-]
-
 const CardList = (props) => {
   return (
     <div>
@@ -49,15 +37,55 @@ const CardList = (props) => {
   )
 }
 
-
-
+class Form extends React.Component {
+  state = {userName:''}
+  handleSubmit = (event) => {
+    event.preventDefault();
+   // console.log(this.state.userName);
+      axios.get(`https://api.github.com/users/${this.state.userName}`)
+      .then(resp => {
+        this.props.onSubmit(resp.data);
+      });
+  }
+  render() {
+    //ref = {(input) => this.usernameInput = input}    ------> one way of getting input value using ref
+    return (
+      <form onSubmit = {this.handleSubmit}>
+        <input type="text" value={this.state.userName} onChange = {(event) => this.setState({userName:event.target.value})}  placeholder = "Github username" required/>
+        <button type="submit">Add card</button>
+      </form>
+    )
+  }
+}
 
 class App extends Component {
+   state = {
+     cards : [
+      {
+        name:"RaghottamK",
+        avatar_url:"https://avatars3.githubusercontent.com/u/23549690?v=4",
+        company:"Cerner"
+      },
+      {
+        name:"Pooja Bhaumik",
+        avatar_url:"https://avatars3.githubusercontent.com/u/19394896?v=4",
+        company:"xxCode"
+      }
+    ]
+}
+
+  addNewCard = (cardInfo) => {
+    console.log(cardInfo);
+    this.setState(prevState => ({
+      cards:prevState.cards.concat(cardInfo)
+    }))
+  };
   render() {
     return (
       <div>
         <ReactMain />
-        <CardList cards={data} />
+        <Form onSubmit = {this.addNewCard}/>
+        <CardList cards={this.state.cards} />
       </div>      
     );
   }
