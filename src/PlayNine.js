0fefd1dc706c03b3099b8bcb67df3236ef86a57e
Kulkarni <mypersonallogins@gmail.com>
2018-03-01 18:39:02 +0530

    Play-nine game initial commit

---
+++ src/PlayNine.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+const Stars = (props) => {
+    return (
+        <div>...</div>
+    );
+}
+
+const Button = (props) => {
+    return (
+        <div>...</div>
+    );
+}
+
+const Answer = (props) => {
+    return (
+        <div>...</div>
+    );
+}
+
+class Game extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Play Nine</h3>
+                <Stars />
+                <Button />
+                <Answer />
+            </div>
+        );
+    }
+}
+
+class PlayApp extends Component {
+    render() {
+        return (
+            <Game />
+        )
+    }
+}
+
+export default PlayApp;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import PlayApp from './PlayNine';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const ReactApp = (props) => {
+    return (
+        <div>
+            <App />
+            <PlayApp />
+        </div>
+    );
+  }
+ReactDOM.render(<ReactApp />, document.getElementById('root'));
 registerServiceWorker();
